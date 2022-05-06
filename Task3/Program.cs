c844f9b9aa11064696bdfa6b6afafbdc31ef5252
Ilfat <ilfat95@mail.ru>
2022-05-06 19:39:09 +0500

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task3
+{
+    public delegate double MyDelegate(RandomDelegate[] d);
+    public delegate int RandomDelegate();
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Random rand = new Random();
+            MyDelegate myDelegate = delegate (RandomDelegate[] d)
+            {
+                double sum = new double();
+                for (int i = 0; i < d.Length; i++)
+                {
+                    sum += d[i].Invoke();
+                }
+                return sum / d.Length;
+            };
+            RandomDelegate randomDelegate = delegate () { return rand.Next(100); };
+            RandomDelegate randomDelegate2 = delegate () { return rand.Next(100); };
+
+            RandomDelegate[] randomDelegates = {randomDelegate, randomDelegate2 };
+            Console.WriteLine(myDelegate(randomDelegates));
+
+
+            Console.ReadKey();
+         
+        }
+    }
+}
